17310c73f4c5951570954bf83c3879a200c83886
duccop32 <hoangduc002004@gmail.com>
2026-01-19 18:04:24 +0700

    Add GameRoom and GameInvitation

---
+++ CaroLAN/WinFormServer/GameInvitation.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Net.Sockets;
+
+namespace WinFormServer
+{
+    internal class GameInvitation
+    {
+        public string InvitationId { get; private set; }
+        public Socket Sender { get; set; }
+        public Socket Receiver { get; set; }
+        public string SenderEndPoint { get; set; }
+        public string ReceiverEndPoint { get; set; }
+        public DateTime CreatedTime { get; private set; }
+        public bool IsAccepted { get; set; }
+        public bool IsExpired { get; set; }
+
+        public GameInvitation(Socket sender, Socket receiver)
+        {
+            InvitationId = Guid.NewGuid().ToString("N")[..8].ToUpper();
+            Sender = sender;
+            Receiver = receiver;
+            SenderEndPoint = sender.RemoteEndPoint?.ToString() ?? "Unknown";
+            ReceiverEndPoint = receiver.RemoteEndPoint?.ToString() ?? "Unknown";
+            CreatedTime = DateTime.Now;
+            IsAccepted = false;
+            IsExpired = false;
+        }
+
+        public bool IsValid()
+        {
+            return !IsExpired && (DateTime.Now - CreatedTime).TotalSeconds < 10;
+        }
+    }
+}
---
+++ CaroLAN/WinFormServer/GameRoom.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormServer
+{
+    internal class GameRoom
+    {
+        public string RoomId { get; private set; }
+        public List<Socket> Players { get; private set; }
+        public bool IsGameStarted { get; set; }
+        public DateTime CreatedTime { get; private set; }
+        public int MaxPlayers { get; } = 2;
+
+        public GameRoom(string roomId)
+        {
+            RoomId = roomId;
+            Players = new List<Socket>();
+            IsGameStarted = false;
+            CreatedTime = DateTime.Now;
+        }
+
+        public bool AddPlayer(Socket player)
+        {
+            if (Players.Count < MaxPlayers && !Players.Contains(player))
+            {
+                Players.Add(player);
+                return true;
+            }
+            return false;
+        }
+
+        public void RemovePlayer(Socket player)
+        {
+            Players.Remove(player);
+        }
+
+        public bool IsFull()
+        {
+            return Players.Count >= MaxPlayers;
+        }
+
+        public bool IsEmpty()
+        {
+            return Players.Count == 0;
+        }
+
+        public Socket GetOpponent(Socket player)
+        {
+            return Players.FirstOrDefault(p => p != player);
+        }
+    }
+}
